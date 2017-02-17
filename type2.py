8e390fc944c4fb861ab3ff6d9544fd2b6749ce25
sunilsj99 <rpspjjjj@gmail.com>
2017-02-17 10:29:24 -0800

    second type in python

---
+++ type2.py
@@ -0,0 +1,20 @@
+print "Mary has a little lamb"
+print "its fleece was white as %s" % 'snow'
+print "and everywhere that marry went"
+print "."*10
+
+end1 = "C"
+end2 = "h"
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'B'
+end7 = 'e'
+end8 = 'r'
+end9 = 'g'
+end10 = 'u'
+end11 = 'r'
+
+print end1+end2+end3+end4+end5,
+print end6+end7+end8+end9+end10+end11
+
